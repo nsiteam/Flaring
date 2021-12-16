ef4803656930741413376184d8ceb463948836e7
Scott Roberts <scott@hopkunsunderground.com>
2021-12-16 08:38:44 -0600

    Mongo Update

--- src/main/kotlin/AppVnfGeoExample.kt
+++ src/main/kotlin/AppVnfGeoExample.kt
@@ -11,7 +11,7 @@ import java.io.File
 
 fun main() {
     val client = KMongo.createClient()
-    val db = client.getDatabase("flaring")
+    val db = client.getDatabase("flare")
     val col = db.getCollection<VnfEvent>()
 
     val input = File("data/shp/ne_110m_admin_0_countries.shp")
